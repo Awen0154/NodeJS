392590ac415b33071b0b203fa82459dfd0c9fc4e
Awen0154 <91340428+Awen0154@users.noreply.github.com>
2022-05-25 23:00:27 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('\n_________  __      __  _________\n\\_   ___ \\\/  \\    \/  \\\/   _____\/\n\/    \\  \\\/\\   \\\/\\\/   \/\\_____  \\ \n\\     \\____\\        \/ \/        \\\n \\______  \/ \\__\/\\  \/ \/_______  \/\n        \\\/       \\\/          \\\/ \n');
+console.log('Bienvenue sur votre serveur d\'hébergement NodeJS');
+console.log('Merci d\'avoir fait confiance à CPUHost !');
+console.log('>> Pour installer votre bot, connectez vous au serveur en SFTP');
+console.log('                                            ');                                                                                                              
+
+process.stdin.resume();
